baa494b1c6913f05b8aacac6ae4f575dbc7ff951
Erik Jenkins <ejenkins132@gmail>
2016-01-15 16:31:25 -0500

    test insert

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var assert  = require('assert');
 var express = require('express');
-var mongo   = require('mongodb');
+var mongo   = require('mongodb').MongoClient;
 app = express();
 
 // set up database
 var db;
 var dbURI = 'mongodb://heroku_9dr87f8p:f1jndcla6mb8cps74d7iplass9@ds047075.mongolab.com:47075/heroku_9dr87f8p';
 
-mongo.MongoClient.connect(dbURI, function(err, database) {
+mongo.connect(dbURI, function(err, database) {
   assert.equal(null, err);
 
   db = database;
 });
+
+// test insert
+mongo.collections('test').insertOne({"name": "Erik", "time": new Date()});
 
 app.set('port', (process.env.PORT || 5000));
 
