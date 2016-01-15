cbdd5291b2a580e4f877c965a4619c1cde2d97d0
Erik Jenkins <ejenkins132@gmail>
2016-01-15 16:37:00 -0500

    organized index.js

--- index.js
+++ index.js
@@ -3,23 +3,15 @@ var express = require('express');
 var mongo   = require('mongodb').MongoClient;
 app = express();
 
-// set up database
-var db;
 var dbURI = 'mongodb://heroku_9dr87f8p:f1jndcla6mb8cps74d7iplass9@ds047075.mongolab.com:47075/heroku_9dr87f8p';
-
-mongo.connect(dbURI, function(err, database) {
+mongo.connect(dbURI, function(err, db) {
   assert.equal(null, err);
 
-  db = database;
+  app.set('port', (process.env.PORT || 5000));
 
-  // test insert
-  db.collection('test').insertOne({"name": "Erik", "time": new Date()});
-});
+  app.use(express.static('app'));
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static('app'));
-
-app.listen(app.get('port'), function() {
-  console.log("Express listening on port " + app.get('port'));
+  app.listen(app.get('port'), function() {
+    console.log("Express listening on port " + app.get('port'));
+  });
 });
