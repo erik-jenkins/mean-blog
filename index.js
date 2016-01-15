42253909bea293b1fd083668b15ed0a79978d631
Erik Jenkins <ejenkins132@gmail>
2016-01-15 16:26:54 -0500

    added mongodb

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+var assert  = require('assert');
 var express = require('express');
+var mongo   = require('mongodb');
 app = express();
+
+// set up database
+var db;
+var dbURI = 'mongodb://heroku_9dr87f8p:f1jndcla6mb8cps74d7iplass9@ds047075.mongolab.com:47075/heroku_9dr87f8p';
+
+mongo.MongoClient.connect(config.MONGO_URI, function(err, database) {
+  assert.equal(null, err);
+
+  db = database;
+});
 
 app.set('port', (process.env.PORT || 5000));
 
